e02c6e18c99c6260866315c8d27afd2fd75be20c
Alexandr <104072970+Dennchik@users.noreply.github.com>
2024-03-21 03:01:16 +0300

    21 mrt 2024

--- #src/js/main.js
+++ #src/js/main.js
@@ -1,72 +1,72 @@
 
-// // import Swiper from 'swiper'; 
-// // import anime from 'animejs/lib/anime.es.js';
-// // import Parallax from 'parallax-js';
-// import Rellax from 'rellax';
-// // import Rellax from "./module/rellax";
-// // Rellax();
+// import Swiper from 'swiper'; 
+// import anime from 'animejs/lib/anime.es.js';
+// import Parallax from 'parallax-js';
+import Rellax from 'rellax';
+// import Rellax from "./module/rellax";
+// Rellax();
 
-// // var rellax = new Rellax('.rellax');
-// // console.log(rellax);
-// // var rellax = new Rellax('.rellax', {
-// // 	breakpoints: [576, 768, 1201],
-// // }); 
+// var rellax = new Rellax('.rellax');
+// console.log(rellax);
+// var rellax = new Rellax('.rellax', {
+// 	breakpoints: [576, 768, 1201],
+// }); 
 
-// // var rellax = new Rellax('.rellax', {
-// // 	// center: true
-// // 	callback: function (position) {
-// // 		// callback every position change
-// // 		console.log(position);
-// // 	}
-// // });
-
-// // -----------------------------------------------------------------------------
-
-// function animItems() {
-// 	const animItems = document.querySelectorAll('._rellax');
-// 	const rellaxWrapper = document.querySelector('._rellax-wrapper');
-// 	if (animItems.length > 0) {
-// 		function startHieght() {
-// 			const lastElement = Object.values(animItems).pop();
-// 			const contenBlockHeight = rellaxWrapper.offsetHeight;
-// 			rellaxWrapper.style.height = contenBlockHeight;
-// 			let lastHeight = lastElement.offsetHeight;
-
-// 			function gettHieght() {
-// 				const rellaxWrapperTop = offsets(rellaxWrapper).top;
-// 				const lastElementTop = offsets(lastElement).top;
-
-// 				rellaxWrapper.style.height = lastElementTop - rellaxWrapperTop + lastHeight + 'px';
-// 			}
-// 			return gettHieght;
-// 		}
-
-// 		let _startHieght = startHieght();
-
-// 		let animeRellax = new Rellax('._rellax', {
-// 		});
-// 		animeRellax.refresh();
-
-
-
-
-// 		window.addEventListener('scroll', function () {
-// 			_startHieght();
-// 		});
-
-// 		function offsets(el) {
-// 			const rect = el.getBoundingClientRect(),
-// 				scrollLeft = window.scrollX || document.documentElement.scrollLeft,
-// 				scrollTop = window.scrollY || document.documentElement.scrollTop;
-// 			return { top: rect.top + scrollTop, left: rect.left + scrollLeft };
-// 		}
+// var rellax = new Rellax('.rellax', {
+// 	// center: true
+// 	callback: function (position) {
+// 		// callback every position change
+// 		console.log(position);
 // 	}
-// }
-// animItems();
+// });
 
-// //* ----------------------------------------------------------------------------
-// console.log("%c РОССИЯ ", "background: blue; color: yellow; font-size: x-large; border-left: 5px solid black; border-top: 30px solid white; border-right: 2px solid black; border-bottom: 30px solid red;");
-// //* ----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+
+function animItems() {
+	const animItems = document.querySelectorAll('._rellax');
+	const rellaxWrapper = document.querySelector('._rellax-wrapper');
+	if (animItems.length > 0) {
+		function startHieght() {
+			const lastElement = Object.values(animItems).pop();
+			const contenBlockHeight = rellaxWrapper.offsetHeight;
+			rellaxWrapper.style.height = contenBlockHeight;
+			let lastHeight = lastElement.offsetHeight;
+
+			function gettHieght() {
+				const rellaxWrapperTop = offsets(rellaxWrapper).top;
+				const lastElementTop = offsets(lastElement).top;
+
+				rellaxWrapper.style.height = lastElementTop - rellaxWrapperTop + lastHeight + 'px';
+			}
+			return gettHieght;
+		}
+
+		let _startHieght = startHieght();
+
+		let animeRellax = new Rellax('._rellax', {
+		});
+		animeRellax.refresh();
+
+
+
+
+		window.addEventListener('scroll', function () {
+			_startHieght();
+		});
+
+		function offsets(el) {
+			const rect = el.getBoundingClientRect(),
+				scrollLeft = window.scrollX || document.documentElement.scrollLeft,
+				scrollTop = window.scrollY || document.documentElement.scrollTop;
+			return { top: rect.top + scrollTop, left: rect.left + scrollLeft };
+		}
+	}
+}
+animItems();
+
+//* ----------------------------------------------------------------------------
+console.log("%c РОССИЯ ", "background: blue; color: yellow; font-size: x-large; border-left: 5px solid black; border-top: 30px solid white; border-right: 2px solid black; border-bottom: 30px solid red;");
+//* ----------------------------------------------------------------------------
 // // const obj = { one: 1, two: 2, three: 3 };
 
 // // const lastKey = Object.keys(obj).pop();
